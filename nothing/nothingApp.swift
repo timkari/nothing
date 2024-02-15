60275f67ad813cea1f43b91000c984ac545fea28
timkari <ktm332@mail.ru>
2024-02-16 01:22:52 +0300

    Initial Commit

---
+++ nothing/nothingApp.swift
@@ -0,0 +1,17 @@
+//
+//  nothingApp.swift
+//  nothing
+//
+//  Created by Каримов Тимур on 08.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct nothingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
